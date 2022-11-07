1494132819e544b3bdd941e40afb76c27bafc957
Fabiano Garcia <fabianojp06@gmail.com>
2022-11-07 15:50:48 -0300

    deploy

---
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { Categoria } from '../categoria.model';
+import { CategoriaService } from '../categoria.service';
+
+@Component({
+  selector: 'app-categoria-read',
+  templateUrl: './categoria-read.component.html',
+  styleUrls: ['./categoria-read.component.css']
+})
+export class CategoriaReadComponent implements OnInit {
+
+  categorias: Categoria[] = []
+
+  displayedColumns: string[] = ['id', 'nome', 'descricao', 'livros', 'acoes'];
+  
+
+
+  constructor(private service: CategoriaService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    this.service.findAll().subscribe(reposta => {
+      console.log(reposta);
+      this.categorias = reposta;
+    })
+  }
+
+  navegarParaCategoriaCreate() {
+    this.router.navigate(["categorias/create"])
+  }
+
+}
---
+++ src/app/components/views/categoria/categoria-update/categoria-update.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Categoria } from '../categoria.model';
+import { CategoriaService } from '../categoria.service';
+
+@Component({
+  selector: 'app-categoria-update',
+  templateUrl: './categoria-update.component.html',
+  styleUrls: ['./categoria-update.component.css']
+})
+export class CategoriaUpdateComponent implements OnInit {
+
+  categoria: Categoria = {
+    id: '',
+    nome: '',
+    descricao: ''
+  }
+
+  constructor(
+    private service: CategoriaService, 
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+    this.categoria.id = this.route.snapshot.paramMap.get('id')!
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.categoria.id!).subscribe((reposta) => {
+      this.categoria.nome = reposta.nome;
+      this.categoria.descricao = reposta.descricao;
+    })
+  }
+
+  update(): void {
+    this.service.update(this.categoria).subscribe((resposta) =>{
+    this.router.navigate(['categorias'])
+    this.service.mensagem('Category updated successfully!')
+    }, err => {
+      this.service.mensagem('Validate that all fields are filled in correctly!')
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['categorias'])
+  }
+
+}
